5b6e553df09f8a98a2278f73b79dbc40701f642c
Nerrey <kolud@bk.ru>
2018-01-31 13:19:22 +0300

    seeds

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -3,26 +3,11 @@ alias Nomad.Admin
 alias Nomad.Static
 
 if !Repo.get_by(Admin, name: "admin") do
-  Repo.insert! %Admin{
+  Repo.insert!(%Admin{
     name: "admin",
-    password_hash: Comeonin.Bcrypt.hashpwsalt("password"),
-  }
+    password_hash: Comeonin.Bcrypt.hashpwsalt("password")
+  })
 end
-
-rows =
-[%{
-  type: :main,
-  info: %{a: :b}
-},
-%{
-  type: :about,
-  info: %{a: :b}
-}]
-|> Enum.map(&(&1 |> Map.merge(
-  %{
-    inserted_at: Timex.now,
-    updated_at: Timex.now
-  })))
 
 Repo.delete_all(Static)
 Repo.insert_all(Static, rows)
